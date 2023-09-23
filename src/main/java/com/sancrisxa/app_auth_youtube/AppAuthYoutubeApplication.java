4f5ac247f77ba972b0412098760cc91b467f1ef7
Samuel Xavier <sancrisxa@gmail.com>
2023-09-23 18:10:09 -0300

    first commit

---
+++ src/main/java/com/sancrisxa/app_auth_youtube/AppAuthYoutubeApplication.java
@@ -0,0 +1,13 @@
+package com.sancrisxa.app_auth_youtube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppAuthYoutubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppAuthYoutubeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sancrisxa/app_auth_youtube/AppAuthYoutubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sancrisxa.app_auth_youtube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppAuthYoutubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
